6ec9b7e4f6c8cb4bdbe208c0ccab365b9f723822
Anubhav7S <77920300+Anubhav7S@users.noreply.github.com>
2023-02-13 23:37:57 +0530

    Test changes

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import redirect, render
 
 def index(request):
     return render(request, "index.html")
+def about(request):
+    return render(request, "about.html")    
 # Create your views here.
