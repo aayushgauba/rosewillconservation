063ebc794ac1a6cf92981376055e78cd6aa96a4f
aayushgauba <55630226+aayushgauba@users.noreply.github.com>
2023-02-13 12:21:52 -0600

    Static files

--- rosewillconservation/settings.py
+++ rosewillconservation/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'rosewillconservation/static')
+]
