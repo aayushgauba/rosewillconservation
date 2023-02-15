c36ff4a4d37621ce1d5169489ddce86599b4d5c7
Anubhav7S <77920300+Anubhav7S@users.noreply.github.com>
2023-02-15 23:39:39 +0530

    Update settings.py

--- rosewillconservation/settings.py
+++ rosewillconservation/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'rosewillconservation',
         'USER':'postgres',
-        'PASSWORD':'aayush',
+        'PASSWORD':'postgres',
         'HOST':'localhost'
     }
 }
