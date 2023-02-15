8e9cd1f44c73812c3708dac18ff385f5aabe4995
aayushgauba <55630226+aayushgauba@users.noreply.github.com>
2023-02-15 10:25:00 -0600

    Initial Contact Page

--- pages/views.py
+++ pages/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import redirect, render
 def index(request):
     return render(request, "index.html")
 def about(request):
-    return render(request, "about.html")    
+    return render(request, "about.html")
+def contact(request):
+    return render(request, "contact.html")
 # Create your views here.
