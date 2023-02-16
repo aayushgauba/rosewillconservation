7c9e0ef7e159c1bb5e2da92315227d74170e5808
aayushgauba <55630226+aayushgauba@users.noreply.github.com>
2023-02-15 22:02:03 -0600

    Year Footer live render

--- pages/views.py
+++ pages/views.py
@@ -4,11 +4,15 @@ from .models import Contact
 from datetime import date
 
 def index(request):
-    return render(request, "index.html")
+    yearstring = "© " + str(date.today().year) +" Rosewill Conservation, Inc"
+    return render(request, "index.html", context={'year':yearstring})
 def about(request):
-    return render(request, "about.html")
+    yearstring = "© " + str(date.today().year) +" Rosewill Conservation, Inc"
+    return render(request, "about.html", context={'year':yearstring})
 def contact(request):
+
     form = ContactForm()
+    yearstring = "© " + str(date.today().year) +" Rosewill Conservation, Inc"
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -17,5 +21,5 @@ def contact(request):
             message = request.POST.get('Message')
             dateform = date.today()
             Contact.objects.create(Name = name, Email = email, Message = message, Date = dateform)
-    return render(request, "contact.html", context = {'form':form})
+    return render(request, "contact.html", context = {'form':form, 'year':yearstring})
 # Create your views here.
